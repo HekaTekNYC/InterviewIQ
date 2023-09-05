a99145d43dd64b6d8922171a16753352f6b49ebe
DHolliday1881 <ellyserummel@gmail.com>
2023-09-05 12:09:16 -0400

    mongodb connected

---
+++ backend/db/dbConnect.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+// require('dotenv').config();
+// console.log(process.env.DB_URL);
+
+// async function dbConnect() {
+//   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
+//   mongoose
+//     .connect(process.env.DB_URL, {
+//       //   these are options to ensure that the connection is done properly
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     })
+//     .then(() => {
+//       console.log('Successfully connected to MongoDB Atlas!');
+//     })
+//     .catch((error) => {
+//       console.log('Unable to connect to MongoDB Atlas!');
+//       console.error(error);
+//     });
+// }
+
+const connectDB = async () => {
+  console.log('hello im working');
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    });
+    console.log('Successfully connected to MongoDB Atlas!');
+  } catch (error) {
+    console.error('Unable to connect to MongoDB Atlas!', error);
+  }
+};
+
+module.exports = connectDB;
