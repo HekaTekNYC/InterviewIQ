ec8aa67af79cd8321e2de5582b6182cab820a794
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2023-09-28 09:58:27 -0600

    Styling & Routing additions
    I pulled the flashcardPage into our App.js and created the css needed for the flashcard and the flashcard flip. There are still a couple bugs with it, but i am pushing up so Celeste can pull down.
    
    Co-Authored-By: Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>

--- src/components/dashboard/Public.js
+++ src/components/dashboard/Public.js
@@ -9,19 +9,19 @@ const Public = () => {
               <h1>Welcome to <span className="nowrap">InterviewIQ</span></h1>
           </header>
           <main className="public__main">
-              <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
-              <address className="public__addr">
+              <p>Your free flashcard app!</p>
+              {/* <address className="public__addr">
                   Dan D. Repairs<br />
                   555 Foo Drive<br />
                   Foo City, CA 12345<br />
                   <a href="tel:+15555555555">(555) 555-5555</a>
-              </address>
+              </address> */}
               <br />
-              <p>Owner: Dan Davidson</p>
+              {/* <p>Owner: Dan Davidson</p> */}
           </main>
           <footer>
-              <Link to="/login">Employee Login</Link>
-              <Link to="/dash">Dash page</Link>
+              <Link to="/login">Admin Login </Link>
+              <Link to="/dash">Flashcard page</Link>
           </footer>
       </section>
  
