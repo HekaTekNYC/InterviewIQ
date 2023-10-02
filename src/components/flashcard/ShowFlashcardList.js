f5e71da274b2a6e9aa48f6ff4b60ca2ef6366217
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2023-10-02 15:02:19 -0600

    ShowFlashcardList Component
    Completed ShowFlashcardList Component
    
    Co-Authored-By: Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>

---
+++ src/components/flashcard/ShowFlashcardList.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import '../../index.css'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Flashcard from './Flashcard';
+
+function ShowFlashcardList() {
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3500/api/flashcards')
+      .then((res) => {
+        setFlashcards(res.data);
+      })
+      .catch((err) => {
+        console.log('Error from ShowFlashcardList');
+      });
+  }, []);
+
+  const flashcardList =
+   flashcards.length === 0
+      ? 'there is no flashcard record!'
+      : flashcards.map((flashcard, k) => <FlashCard flashcard={flashcard} key={k} />);
+
+  return (
+    <div className='ShowFlashcardList'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <br />
+            <h2 className='display-4 text-center'>Flashcard List</h2>
+          </div>
+
+          <div className='col-md-11'>
+            <Link
+              to='/create-flashcard'
+              className='btn btn-outline-warning float-right'
+            >
+              + Add New Flashcard
+            </Link>
+            <br />
+            <br />
+            <hr />
+          </div>
+        </div>
+
+        <div className='list'>{flashcardList}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ShowFlashcardList;
