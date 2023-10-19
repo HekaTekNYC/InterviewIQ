9ffc9565643c87497cea91a424b808ab5fb5bf9e
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2023-10-18 22:16:46 -0400

    backend fetching
    created http hook and implemented category fetching. Current bug in abortControl with http hook canceling fetch.
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.css';
-import 'tailwindcss/tailwind.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import "tailwindcss/tailwind.css";
 
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,61 @@
-import React from 'react'
-import NavbarHome from '../components/navbar/navbarHome/NavbarHome'
-import ShowCategoryCardList from '../components/categories/showCategoryCardList/ShowCategoryCardList'
-import ShowSubCardList from '../components/subcategories/showSubCardList/showSubCardList'
+import React, { useState, useEffect, useRef } from "react";
+import NavbarHome from "../components/navbar/navbarHome/NavbarHome";
+import CategoryList from "../components/categories/categoryList/CategoryList";
+import ErrorModal from "../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../shared/components/hooks/httpHook";
+// import ShowSubCardList from "../components/subcategories/showSubCardList/showSubCardList";
+// import Login from "../components/Login";
+// import Register from "../components/Register";
 
 const Home = () => {
-  return (
-   <>
-    <NavbarHome />
-    
-    {/* <div className='bg-gradient-to-r from-blue-200 to-blue-100'> */}
-    <div className='bg-white-200'>
-      <div className="py-20 p-4 header-container flex-row">
-        <h1 className='text-center text-3xl'>Pick A Category</h1>
-      </div>
-      <div className="card-list">
-        <ShowCategoryCardList/>
-          {/* <ShowSubCardList/> */}
-      </div>
-    
-      </div>
-    
-    </>
-  )
-}
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedCategories, setLoadedCategories] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        console.log("Before fetch request");
+        const responseData = await sendRequest(
+          "http://localhost:3500/api/categories"
+        );
+        console.log("responseData in Home", responseData);
+        const categories = responseData.categories;
+
+        console.log("Categories before setting state:", categories);
+
+        setLoadedCategories(categories);
+      } catch (err) {
+        // Handle errors
+      }
+    };
+    fetchCategories();
+  }, [sendRequest]);
+
+  return (
+    <>
+      <NavbarHome />
+      {/* <Login /> */}
+
+      {/* <div className='bg-gradient-to-r from-blue-200 to-blue-100'> */}
+      <div className="bg-white-200">
+        <div className="py-20 p-4 header-container flex-row">
+          <h1 className="text-center text-3xl">Pick A Category</h1>
+        </div>
+        <div className="card-list">
+          <ErrorModal error={error} onClear={clearError} />
+          {isLoading && (
+            <div className="center">
+              <LoadingSpinner />
+            </div>
+          )}
+          {!isLoading && loadedCategories && (
+            <CategoryList categories={loadedCategories} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
