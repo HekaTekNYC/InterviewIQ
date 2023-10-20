dfa1d21c5b7903cf9eeeeae9b6c3305b141418b6
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2023-10-20 11:47:26 -0600

    UI Alterations
    Minor UI changes
    
    Co-Authored-By: Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>

--- src/components/categories/categoryCard/CategoryCard.js
+++ src/components/categories/categoryCard/CategoryCard.js
@@ -3,6 +3,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CategoryCard = (props) => {
+  const cardColors = [
+    "bg-red-300",
+    "bg-green-300",
+    "bg-blue-300",
+    "bg-pink-300",
+    "bg-yellow-300",
+  ];
+
+    // Get the color class for the current card based on its index
+    const cardColorClass = cardColors[props.index % cardColors.length];
+
   return (
     <div className="bg-white p-4 m-2 rounded-lg shadow-md hover:shadow-x1">
       <Link to={`/${props.id}/categories`}>
