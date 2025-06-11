79812b5ff483aacdfcef4061994a96b6e2c8d50f
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-06-11 10:38:40 -0400

    backup
    creating a backup before gutting and cleaning

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { Routes, Route, Outlet, Navigate } from "react-router-dom";
-import { useState, useCallback } from "react";
-import Home from "./pages/Home";
+import {Routes, Route, Outlet, Navigate} from "react-router-dom"
+import React, {useState, useCallback} from "react"
+import Home from "./pages/Home"
 
-import Auth from './user/pages/Auth';
+import Auth from "./user/pages/Auth"
 
-import { AuthContext } from "./shared/context/auth-context";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import Register from "./components/Register";
-import UpdateFlashcard from './components/flashcard/updateFlashcard/UpdateFlashcard';
-import UsersFlashcards from './components/flashcard/pages/UsersFlashcards';
-import NewFlashcard from './components/flashcard/pages/NewFlashcard';
+import {AuthContext} from "./shared/context/auth-context"
+import MainNavigation from "./shared/components/Navigation/MainNavigation"
+import Register from "./components/Register"
+import UpdateFlashcard from "./components/flashcard/updateFlashcard/UpdateFlashcard"
+import UsersFlashcards from "./components/flashcard/pages/UsersFlashcards"
+import NewFlashcard from "./components/flashcard/pages/NewFlashcard"
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userId, setUserId] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userId, setUserId] = useState(false)
 
-  const login = useCallback((uid) => {
-    setIsLoggedIn(true);
-    setUserId(uid);
-  }, []);
+  const login = useCallback(uid => {
+    setIsLoggedIn(true)
+    setUserId(uid)
+  }, [])
 
   const logout = useCallback(() => {
-    setIsLoggedIn(false);
-    setUserId(null);
-  }, []);
+    setIsLoggedIn(false)
+    setUserId(null)
+  }, [])
 
   return (
     <AuthContext.Provider
@@ -31,7 +31,7 @@ function App() {
         isLoggedIn: isLoggedIn,
         userId: userId,
         login: login,
-        logout: logout
+        logout: logout,
       }}
     >
       <MainNavigation />
@@ -39,141 +39,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/flashcards/new" element={<NewFlashcard />} />
         <Route path="/flashcards/:userId" element={<UpdateFlashcard />} />
-        <Route path="/auth/*" element={<Auth />} />
         <Route path="/register" element={<Register />} />
         <Route path="/:userId/flashcards/*" element={<UsersFlashcards />} />
         <Route path="*" element={<Navigate to="/auth/login" />} />
       </Routes>
     </AuthContext.Provider>
-  );
+  )
 }
 
-export default App;
-
-
-
-
-
-
-
-// import { Routes, Route, Navigate, Switch } from "react-router-dom";
-// import Home from "./pages/Home";
-// import "./pages/home.styles.css";
-// import CategoryList from "./components/categories/categoryList/CategoryList";
-// // import CreateFlashcard from "./components/flashcard/createFlashcard/CreateFlashcard";
-// // import UpdateFlashcard from "./components/flashcard/updateFlashcard/UpdateFlashcard";
-// // import Flashcard from "./components/flashcard/flashcardComponent/Flashcard";
-// import Login from "./components/navbar/navbarHome/LogIn";
-// import SubList from "./components/subcategories/subList/subList"
-// import Users from "./user/pages/Users";
-// import NewFlashcard from './components/flashcard/pages/NewFlashcard'
-// import UserFlashcards from './components/flashcard/pages/UserFlashcards'
-// import UpdateFlashcard from './components/flashcard/pages/UpdateFlashcard'
-// import Auth from './components/user/pages/Auth';
-// import { AuthContext } from './shared/context/auth-context'
-// import MainNavigation from './shared/components/Navigation/MainNavigation'
-
-// import Register from "./components/Register";
-
-// function App() {
-
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   const login = useCallback(() => {
-//     setIsLoggedIn(true);
-//   }, []);
-
-//   const logout = useCallback(() => {
-//     setIsLoggedIn(false);
-//   }, []);
-
-//   let router;
-
-//   if (isLoggedIn) {
-//     router = (
-//       <Switch>
-//         <Route path="/" exact>
-//           <Users />
-//         </Route>
-//         <Route path="/:userId/places" exact>
-//           <UserFlashcards />
-//         </Route>
-//         <Route path="/places/new" exact>
-//           <NewFlashcard />
-//         </Route>
-//         <Route path="/places/:placeId">
-//           <UpdateFlashcard />
-//         </Route>
-//         <Navigate to="/" />
-//       </Switch>
-//     );
-//   } else {
-//     router = (
-//       <Switch>
-//         <Route path="/" exact>
-//           <Users />
-//         </Route>
-//         <Route path="/:userId/flashcards" exact>
-//           <UserFlashcards />
-//         </Route>
-//         <Route path="/auth">
-//           <Auth />
-//         </Route>
-//         <Navigate to="/auth" />
-//       </Switch>
-//     );
-//   }
-
-
-//   return (
-//     <AuthContext.Provider
-//       value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
-//     >
-//       <Routes>
-//         <MainNavigation />
-//         <main>{router}</main>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/:categoryId/categories" element={<CategoryList />} />
-//         <Route path="/api/subcategories" element={<SubList />} />
-
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="*" element={<Navigate to="/" />} />
-//       </Routes>
-//       </AuthContext.Provider>
-//   );
-// }
-
-// export default App;
-
-// import { BrowserRouter as Router, Route, Navigate } from "react-router-dom";
-// import Home from "./pages/Home";
-// import "./pages/home.styles.css";
-// import CreateFlashcard from "./components/flashcard/createFlashcard/CreateFlashcard";
-// import UpdateFlashcard from "./components/flashcard/updateFlashcard/UpdateFlashcard";
-// import Flashcard from "./components/flashcard/flashcardComponent/Flashcard";
-// import Login from "./components/Login";
-// import Register from "./components/Register";
-// import SubList from './components/subcategories/subList/subList';
-// import Auth from './user/pages/Auth';
-// import MainNavigation from './components/Navigation/MainNavigation';
-
-// function App() {
-//   return (
-//     <div>
-//       {/* <NavBar/> */}
-//       <Router>
-//         <Route path="/" exact>
-//           <Home />
-//           <Navigate replace to="/home" />
-//         </Route>
-
-//         {/* <Route path="/create-flashcard" element={<CreateFlashcard />} />
-//         <Route path="/update-flashcard" element={<UpdateFlashcard />} />
-//         <Route path="/edit-flashcard/:id" element={<Flashcard />} /> */}
-//       </Router>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App
--- src/components/categories/categoryList/CategoryList.js
+++ src/components/categories/categoryList/CategoryList.js
@@ -1,53 +1,43 @@
-import React from "react";
-import CategoryCard from "../categoryCard/CategoryCard";
-import { Link } from "react-router-dom";
-import "../../../index.css";
-import "./categoryList.styles.css";
-
-
-
-
-
+import React from "react"
+import CategoryCard from "../categoryCard/CategoryCard"
+import {Link} from "react-router-dom"
+import "../../../index.css"
+import "./categoryList.styles.css"
 
 const CategoryList = () => {
-
   // Category card data
   const categories = [
     {
-      id: 'c1',
-      name: 'Frontend',
-      subCount: '30'
+      id: "c1",
+      name: "Frontend",
+      subCount: "30",
     },
     {
-      id: 'c2',
-      name: 'Backend',
-      subCount: '20'
+      id: "c2",
+      name: "Backend",
+      subCount: "20",
     },
     {
-      id: 'c3',
-      name: 'Languages',
-      subCount: '26'
+      id: "c3",
+      name: "Languages",
+      subCount: "26",
     },
     {
-      id: 'c4',
-      name: 'Behavioral',
-      subCount: '37'
+      id: "c4",
+      name: "Behavioral",
+      subCount: "37",
     },
-
   ]
 
-
-
   if (!categories || categories.length === 0) {
-    return <p>No categories available</p>;
+    return <p>No categories available</p>
   }
   // handleclick needs to go here
 
   return (
-
     <>
       <div className="grid grid-cols-4 gap-4">
-        {categories.map((category) => (
+        {categories.map(category => (
           <div key={category.id}>
             <CategoryCard
               id={category.id}
@@ -58,68 +48,7 @@ const CategoryList = () => {
         ))}
       </div>
     </>
+  )
+}
 
-  );
-};
-
-export default CategoryList;
-
-// import React from "react";
-// import CategoryCard from "../categoryCard/CategoryCard";
-// import "../../../index.css";
-// import "./categoryList.styles.css";
-
-// const CategoryList = ({ categories }) => {
-//   // handleclick needs to go here
-//   return (
-//     <>
-//       <div className="grid grid-cols-3 gap-4">
-//         {categories.map((category) => (
-//           <div key={category._id}>
-//             <CategoryCard
-//               id={category._id}
-//               name={category.name}
-//               categoryCount={category.subcategories.length}
-//             />{" "}
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default CategoryList;
-
-// import React from "react";
-// import CategoryCard from "../categoryCard/CategoryCard";
-// import "../../../index.css";
-// import "./categoryList.styles.css";
-
-// function CategoryList(props) {
-//   if (props.categories.length === 0) {
-//     console.log("Category Props:", props.categories);
-//     return (
-//       <div className="center">
-//         <h2>Loading...</h2>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <ul className="categories-list">
-//       {props.categories.map((category) => {
-//         console.log("Category data:", category); // Log each category's data
-//         return (
-//           <CategoryCard
-//             key={category.id}
-//             id={category._id}
-//             name={category.name}
-//             categoryCount={props.categories.length}
-//           />
-//         );
-//       })}
-//     </ul>
-//   );
-// }
-
-// export default CategoryList;
+export default CategoryList
